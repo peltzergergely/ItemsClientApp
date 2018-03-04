8e7c47ed1cca53ec7a173cbfa4e9913206b69ccb
peltzergergely <peltzer.gergely@gmail.com>
2018-03-04 18:08:38 +0100

    Working Client for the Warehouse API
    After launching the server this would try and do requests on port 5000

---
+++ ItemsClientApp/Items.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ItemsClientApp
+{
+    public class Item
+    {
+        [DataMember(Name = "Id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "Name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "Owner")]
+        public string Owner { get; set; }
+
+        [DataMember(Name = "Pos")]
+        public int Pos { get; set; }
+    }
+}
